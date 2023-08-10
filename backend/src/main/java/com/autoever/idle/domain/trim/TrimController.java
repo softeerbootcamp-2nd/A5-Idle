4f33e24560dd500dfa1f86c9277b05670f84a1d9
Hypersand <on8214@gmail.com>
2023-08-10 14:03:37 +0900

    [fix] merge 충돌 수정

--- backend/src/main/java/com/autoever/idle/domain/trim/TrimController.java
+++ backend/src/main/java/com/autoever/idle/domain/trim/TrimController.java
@@ -3,7 +3,10 @@ package com.autoever.idle.domain.trim;
 import com.autoever.idle.domain.trim.dto.TrimSelectionResDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
