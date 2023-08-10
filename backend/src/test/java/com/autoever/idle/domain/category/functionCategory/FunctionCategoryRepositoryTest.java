3e04f0aae20bb42aae08740c040c6ffdf90239c0
Jeongyeon Kim <wjddus9132@gmail.com>
2023-08-10 14:10:02 +0900

    [test] 트림과 카테고리의 기본 옵션 조회 테스트

--- backend/src/test/java/com/autoever/idle/domain/category/functionCategory/FunctionCategoryRepositoryTest.java
+++ backend/src/test/java/com/autoever/idle/domain/category/functionCategory/FunctionCategoryRepositoryTest.java
@@ -1,6 +1,9 @@
 package com.autoever.idle.domain.category.functionCategory;
 
 import com.autoever.idle.domain.category.functionCategory.dto.FunctionCategoryDto;
+import com.autoever.idle.domain.function.dto.DefaultFunctionResDto;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,20 +11,36 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
-
 @SpringBootTest
 @DisplayName("Function Category Repository Test")
 class FunctionCategoryRepositoryTest {
 
+    SoftAssertions softAssertions;
+
     @Autowired
     FunctionCategoryRepository functionCategoryRepository;
+
+    @BeforeEach
+    void beforeEach() {
+        softAssertions = new SoftAssertions();
+    }
 
     @Test
     @DisplayName("전체 옵션 카테고리(8개)를 반환한다")
     void findAll() {
         List<FunctionCategoryDto> categories = functionCategoryRepository.findAll();
 
-        assertThat(categories.size()).isEqualTo(8);
+        softAssertions.assertThat(categories.size()).isEqualTo(8);
+    }
+
+    @Test
+    @DisplayName("해당 트림과 카테고리의 기본 옵션을 반환한다.")
+    void getDefaultOptions() {
+        Long trimId = 1L;
+        Long categoryId = 1L;
+
+        List<DefaultFunctionResDto> defaultFunctions = functionCategoryRepository.getDefaultOptions(trimId, categoryId);
+
+        softAssertions.assertThat(defaultFunctions.size()).isEqualTo(5);
     }
 }
